c6837c5fe2a82236485548df00bdbb15b19a4df5
Alexander <sphc@yandex.ru>
2017-10-25 14:29:24 +0300

    Kotlin Singleton Realisation

--- src/phoperator/core/PHOperator.kt
+++ src/phoperator/core/PHOperator.kt
@@ -1,37 +1,13 @@
-package phoperator.core
+package ru.fortgroup.dpru.utils
 
 typealias Subscriber<T> = (T, String?) -> Unit
 
 /**
- * Created by Alex on 23.03.2017.
+ * Created by a.minkin on 25.10.2017.
  */
-class PHOperator:IOperator {
-
+object KEventDispatcher:IOperator {
     private val subscribers = HashMap<String, ArrayList<Subscriber<Any?>>>()
     private val _priorityListeners by lazy {HashMap<Subscriber<Any?>, Int>()}
-
-    //-------- SINGLETON REALIZATION -----////
-    private constructor()
-    init { println("This ($this) is a SINGLETON was INITIALIZED") }
-    private object Holder { val INSTANCE = PHOperator() }
-    companion object {
-        private val instance: PHOperator by lazy { Holder.INSTANCE }
-
-        fun <T : Any> subscribe(notif: String, sub: (T, String?) -> Unit, priority:Int = 0) {
-            instance.subscribe(notif, sub, priority)
-        }
-        fun <T : Any> unsubscribe(notif: String, sub:(T, String?) -> Unit){
-            instance.unsubscribe(notif, sub)
-        }
-        fun call(notif: String, data: Any?){
-            instance.call(notif, data)
-        }
-    }
-    //-----------------------------------///
-
-    override fun hasSubscribers(notif: String): Boolean {
-        return this.subscribers[notif] != null
-    }
 
     override fun <T : Any> subscribe(notif: String, sub: (T, String?) -> Unit, priority: Int) {
         val ls = this.subscribers.getOrPut(notif) { ArrayList() }
@@ -42,7 +18,7 @@ class PHOperator:IOperator {
         }
     }
 
-    override fun <T : Any> unsubscribe(notif: String, sub:(T, String?) -> Unit) {
+    override fun <T : Any> unsubscribe(notif: String, sub: (T, String?) -> Unit) {
         val ls = this.subscribers[notif]
         _priorityListeners.remove(sub)
         if (ls != null) {
@@ -57,6 +33,10 @@ class PHOperator:IOperator {
     override fun call(notif: String?, data: Any?) {
         val ls = this.subscribers[notif!!]
         ls?.forEach { it(data, notif) } ?: println("NO LISTENERS FOR EVENT '$notif'")
+    }
+
+    override fun hasSubscribers(notif: String): Boolean {
+        return this.subscribers[notif] != null
     }
 }
 
