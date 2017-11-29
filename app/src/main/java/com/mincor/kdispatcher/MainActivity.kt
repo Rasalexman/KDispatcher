ba167fa2f8f557c36396a15180d8db1065b176c9
Alexander <sphc@yandex.ru>
2017-11-29 20:11:02 +0300

    android project

---
+++ app/src/main/java/com/mincor/kdispatcher/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.mincor.kdispatcher
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mincor.kdispatcher.core.KDispatcher
+
+class MainActivity : AppCompatActivity() {
+
+    val EVENT_CALL_ONE: String = "CALL_NUMBER_TWO"
+    val callbackOne = this::nextFun
+    val callbackTwo = this::testFun
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val test = MyTest()
+
+        KDispatcher.subscribe(EVENT_CALL_ONE, callbackOne, 2)
+        KDispatcher.subscribe(EVENT_CALL_ONE, callbackTwo, 1)
+
+        KDispatcher.call("TEST_CALL", "String")
+        KDispatcher.call(EVENT_CALL_ONE, test)
+
+        KDispatcher.unsubscribe(EVENT_CALL_ONE, callbackOne)
+        KDispatcher.call(EVENT_CALL_ONE, test)
+    }
+
+    fun testFun(data: Any?, str: String? = null) {
+        println("testFun INVOKED With EVENT $str")
+    }
+
+    fun nextFun(data: Any?, str: String? = null) {
+        when (data) {
+            is MyTest -> println("MYTEST SI COMING $str")
+        }
+    }
+
+    fun testOne(e: Any?): Unit {
+        println("testOne FROM MY CLASS $e")
+    }
+
+    fun testTwo(e: Any?) {
+        println("testTwo FROM MY CLASS $e")
+    }
+
+    inner class MyTest {
+        init {
+
+        }
+
+        fun testOne(e: Any?): Unit {
+            println("testOne FROM MY CLASS $e")
+        }
+
+        fun testTwo(e: Any?) {
+            println("testTwo FROM MY CLASS $e")
+        }
+    }
+}
+
+
--- app/src/main/java/com/mincor/kdispatcher/core/KDispatcher.kt
+++ app/src/main/java/com/mincor/kdispatcher/core/KDispatcher.kt
@@ -1,30 +1,30 @@
-package ru.fortgroup.dpru.utils
+package com.mincor.kdispatcher.core
 
 typealias Subscriber<T> = (T, String?) -> Unit
 
 /**
  * Created by a.minkin on 25.10.2017.
  */
-object KEventDispatcher:IOperator {
+object KDispatcher: IDispatcher {
     private val subscribers = HashMap<String, ArrayList<Subscriber<Any?>>>()
     private val _priorityListeners by lazy {HashMap<Subscriber<Any?>, Int>()}
 
     override fun <T : Any> subscribe(notif: String, sub: (T, String?) -> Unit, priority: Int) {
-        val ls = this.subscribers.getOrPut(notif) { ArrayList() }
+        val ls = subscribers.getOrPut(notif) { ArrayList() }
         _priorityListeners.getOrPut(sub as Subscriber<Any?>) {priority}
         if (ls.indexOf(sub) < 0) {
             ls.add(sub)
-            ls.sortBy({_priorityListeners.getOrPut(it) {0}})
+            ls.sortBy({ _priorityListeners.getOrPut(it) {0}})
         }
     }
 
     override fun <T : Any> unsubscribe(notif: String, sub: (T, String?) -> Unit) {
-        val ls = this.subscribers[notif]
+        val ls = subscribers[notif]
         _priorityListeners.remove(sub)
         if (ls != null) {
             if (ls.remove(sub)) {
                 if (ls.isEmpty()) {
-                    this.subscribers.remove(notif)
+                    subscribers.remove(notif)
                 }
             }
         }
@@ -32,21 +32,21 @@ object KEventDispatcher:IOperator {
 
     override fun unsubscribeAll(notif: String) {
         if(hasSubscribers(notif)){
-            this.subscribers.remove(notif)
+            subscribers.remove(notif)
         }
     }
 
     override fun call(notif: String?, data: Any?) {
-        val ls = this.subscribers[notif!!]
+        val ls = subscribers[notif!!]
         ls?.forEach { it(data, notif) } ?: println("NO LISTENERS FOR EVENT '$notif'")
     }
 
     override fun hasSubscribers(notif: String): Boolean {
-        return this.subscribers[notif] != null
+        return subscribers[notif] != null
     }
 }
 
-interface IOperator {
+interface IDispatcher {
     fun <T : Any> subscribe(notif:String, sub:(T, String?)->Unit, priority:Int = 0):Unit
     fun <T : Any> unsubscribe(notif:String, sub:(T, String?)->Unit):Unit
     fun unsubscribeAll(notif:String):Unit
