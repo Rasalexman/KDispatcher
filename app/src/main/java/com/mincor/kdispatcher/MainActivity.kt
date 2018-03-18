42d824c64b7861ab110a9cfe86041b7353692b3e
Alexander <sphc@yandex.ru>
2018-03-19 01:54:34 +0300

    library arr publish

--- app/src/main/java/com/mincor/kdispatcher/MainActivity.kt
+++ app/src/main/java/com/mincor/kdispatcher/MainActivity.kt
@@ -6,23 +6,30 @@ import com.rasalexman.kdispatcher.KDispatcher
 
 class MainActivity : AppCompatActivity() {
 
-    val EVENT_CALL_ONE: String = "CALL_NUMBER_TWO"
-    val callbackOne = this::nextFun
-    val callbackTwo = this::testFun
+    private val EVENT_CALL_ONE: String = "CALL_NUMBER_TWO"
+    private val callbackOne = this::nextFun
+    private val callbackTwo = this::testFun
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val test = MyTest()
 
-        KDispatcher.subscribe(EVENT_CALL_ONE, callbackOne, 2)
-        KDispatcher.subscribe(EVENT_CALL_ONE, callbackTwo, 1)
+        scopeOperation(test)
 
         KDispatcher.call("TEST_CALL", "String")
         KDispatcher.call(EVENT_CALL_ONE, test)
 
         KDispatcher.unsubscribe(EVENT_CALL_ONE, callbackOne)
         KDispatcher.call(EVENT_CALL_ONE, test)
+        KDispatcher.unsubscribeAll(EVENT_CALL_ONE)
+        KDispatcher.call(EVENT_CALL_ONE, test)
+    }
+
+    private fun scopeOperation(test:MyTest){
+        KDispatcher.subscribe(EVENT_CALL_ONE, callbackOne, 2)
+        KDispatcher.subscribe(EVENT_CALL_ONE, callbackTwo, 1)
+        KDispatcher.subscribe(EVENT_CALL_ONE, test::testOne, 3)
     }
 
     fun testFun(data: Any?, str: String? = null) {
@@ -31,12 +38,12 @@ class MainActivity : AppCompatActivity() {
 
     fun nextFun(data: Any?, str: String? = null) {
         when (data) {
-            is MyTest -> println("MYTEST SI COMING $str")
+            is MyTest -> println("nextFun MY TEST IS COMING $str ${data.testOne(data, str)}")
         }
     }
 
-    fun testOne(e: Any?): Unit {
-        println("testOne FROM MY CLASS $e")
+    fun testOne(data: Any?, str: String? = null): Unit {
+        println("testOne FROM MY CLASS $data")
     }
 
     fun testTwo(e: Any?) {
@@ -48,8 +55,8 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        fun testOne(e: Any?): Unit {
-            println("testOne FROM MY CLASS $e")
+        fun testOne(data: Any?, str: String? = null): Unit {
+            println("testOne FROM MyTest CLASS $data")
         }
 
         fun testTwo(e: Any?) {
