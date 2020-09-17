b36086ecfd85efb594e9f10ed175dd27f03ec220
Alexander <sphc@yandex.ru>
2020-09-17 23:08:35 +0300

    release version 1.1.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,11 +24,6 @@ android {
         versionCode = appdependencies.Builds.App.VERSION_CODE
         versionName = appdependencies.Builds.App.VERSION_NAME
         testInstrumentationRunner = "android.support.test.runner.AndroidJUnitRunner"
-
-        buildConfigField("String", "ApiKey", "\"026a257e7842ac9cac1fa627496b1468\"")
-        buildConfigField("String", "IMAGES_URL", "\"https://image.tmdb.org/t/p/w500\"")
-        buildConfigField("String", "SERVER_URL", "\"https://api.themoviedb.org/3/\"")
-        buildConfigField("String", "IMAGES_BACKDROP_URL", "\"https://image.tmdb.org/t/p/original\"")
     }
     buildTypes {
         getByName("debug") {
